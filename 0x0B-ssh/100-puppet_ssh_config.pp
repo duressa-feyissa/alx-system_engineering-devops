3d927c884a7c0868c87ec5e269d52dd9b3172c56
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-10-31 20:19:15 +0300

    axl

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#Your SSH client configuration must be configured to use the private key ~/.ssh/school
+#Your SSH client configuration must be configured to refuse to authenticate using a password
+
+file_line { 'Turn off password authentication':
+	ensure => 'present',
+	path   => '/etc/ssh_config',
+	line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+	ensure => 'present',
+	path   => '/etc/ssh_config',
+	line   => '    IdentitiyFile ~/.ssh/school',
+}
