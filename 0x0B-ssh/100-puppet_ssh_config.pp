a78546dcebd8a8637a996cf489f9cc6c4499a367
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-10-31 20:21:29 +0300

    axl

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,13 +2,12 @@
 #Your SSH client configuration must be configured to refuse to authenticate using a password
 
 file_line { 'Turn off password authentication':
-	ensure => 'present',
-	path   => '/etc/ssh_config',
-	line   => '    PasswordAuthentication no',
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
 }
-
 file_line { 'Declare identity file':
-	ensure => 'present',
-	path   => '/etc/ssh_config',
-	line   => '    IdentitiyFile ~/.ssh/school',
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentitiyFile ~/.ssh/school',
 }
