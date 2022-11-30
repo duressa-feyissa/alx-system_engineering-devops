e1067fdf1fa5cca1c6e2b5c9af342460f23ca2f6
duressa-feyissa <duresafeyisa2022@gmail.com>
2022-12-01 02:05:19 +0300

    alx

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,9 @@ def number_of_subscribers(subreddit):
     if subreddit is None:
         return (0)
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"user-agent": 'API Project by andreshugueth'}
+    x = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; /rv:107.0) "
+    x += "Gecko/20100101 Firefox/107.0"
+    headers = {"user-agent": x}
     r = requests.get(url, headers=headers).json()
     subscribers = r.get("data", {}).get("subscribers", 0)
     return subscribers
